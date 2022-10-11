c2921e14257bbb88f6c243dda39df277f30d26f4
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2022-10-11 18:38:38 -0400

    week5 lab done

--- config/config.js
+++ config/config.js
@@ -2,5 +2,6 @@
 config.js YUN KI JUNG - 301189435 - Oct 01, 2022
 */
 
-
 export const Secret = "MySecret";
+//export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://yunki:yunki1234@cluster0.c6hy5f6.mongodb.net/media?retryWrites=true&w=majority"
